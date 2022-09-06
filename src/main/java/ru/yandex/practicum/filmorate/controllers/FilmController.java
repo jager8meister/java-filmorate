42ae3a857057999856bcaf1b3046ea01a16c2cb9
danil/// <d@d.com>
2022-09-06 13:30:15 +0300

    added response body to both controllers

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,19 +14,27 @@ public class FilmController {
     private Map<Integer, Film> filmMap = new HashMap<>();
 
     @PostMapping(value = "/add")
-    public Film addFilm(@RequestBody Film film) {
-        filmMap.put(film.getId(), film);
-        return film;
+    public @ResponseBody ResponseEntity<Film> addFilm(@RequestBody Film film) {
+        if (filmMap.containsKey(film.getId())) {
+            return new ResponseEntity<>(film, HttpStatus.IM_USED);
+        } else {
+            filmMap.put(film.getId(), film);
+            return new ResponseEntity<>(film, HttpStatus.OK);
+        }
     }
 
     @PutMapping(value = "/update")
-    public Film updateFilm(@RequestBody Film film) {
-        filmMap.put(film.getId(), film);
-        return film;
+    public @ResponseBody ResponseEntity<Film> updateFilm(@RequestBody Film film) {
+        if (filmMap.containsKey(film.getId())) {
+            filmMap.put(film.getId(), film);
+            return new ResponseEntity<>(film, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(film, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping(value = "/all")
-    public Map<Integer, Film> getAll() {
-        return filmMap;
+    public @ResponseBody ResponseEntity<Map<Integer, Film>> getAll() {
+        return new ResponseEntity<>(filmMap, HttpStatus.OK);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.HashMap;
@@ -12,19 +15,27 @@ public class UserController {
     private Map<Integer, User> userMap = new HashMap<>();
 
     @PostMapping(value = "/add")
-    public User addUser(@RequestBody User user) {
-        userMap.put(user.getId(), user);
-        return user;
+    public @ResponseBody ResponseEntity<User> addUser(@RequestBody User user) {
+        if (userMap.containsKey(user.getId())) {
+            return new ResponseEntity<>(user, HttpStatus.IM_USED);
+        } else {
+            userMap.put(user.getId(), user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
     }
 
     @PutMapping(value = "/update")
-    public User updateUser(@RequestBody User user) {
-        userMap.put(user.getId(), user);
-        return user;
+    public @ResponseBody ResponseEntity<User> updateUser(@RequestBody User user) {
+        if (userMap.containsKey(user.getId())) {
+            userMap.put(user.getId(), user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping(value = "/all")
-    public Map<Integer, User> getAll() {
-        return userMap;
+    public @ResponseBody ResponseEntity<Map<Integer, User>> getAll() {
+        return new ResponseEntity<>(userMap, HttpStatus.OK);
     }
 }
