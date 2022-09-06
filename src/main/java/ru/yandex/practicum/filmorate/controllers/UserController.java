1e62d6604eebe5a3aa106c05d9731bdebf2ef984
danil/// <d@d.com>
2022-09-07 02:59:59 +0300

    user controller

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -27,33 +27,34 @@ public class UserController {
         return user;
     }
 
+    private ResponseEntity<User> checkAndSend(User user) {
+        try {
+            if (UserValidator.valid(user)) {
+                user = checkName(user);
+                userMap.put(user.getId(), user);
+                return new ResponseEntity<>(user, HttpStatus.OK);
+            }
+        } catch (ValidationException e) {
+            log.error(e.toString());
+        }
+        return new ResponseEntity<>(user, HttpStatus.NOT_ACCEPTABLE);
+    }
+
     @PostMapping(value = "/add")
     public @ResponseBody ResponseEntity<User> addUser(@RequestBody User user) throws ValidationException {
         if (userMap.containsKey(user.getId())) {
             return new ResponseEntity<>(user, HttpStatus.IM_USED);
         } else {
-            if (UserValidator.valid(user)) {
-                user = checkName(user);
-                userMap.put(user.getId(), user);
-                return new ResponseEntity<>(user, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(user, HttpStatus.NOT_ACCEPTABLE);
-            }
+            return checkAndSend(user);
         }
     }
 
     @PutMapping(value = "/update")
     public @ResponseBody ResponseEntity<User> updateUser(@RequestBody User user) throws ValidationException {
         if (userMap.containsKey(user.getId())) {
-            if (UserValidator.valid(user)) {
-                user = checkName(user);
-                userMap.put(user.getId(), user);
-                return new ResponseEntity<>(user, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(user, HttpStatus.NOT_ACCEPTABLE);
-            }
+            return checkAndSend(user);
         } else {
-            return new ResponseEntity<>(user, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(user, HttpStatus.NOT_ACCEPTABLE);
         }
     }
 
