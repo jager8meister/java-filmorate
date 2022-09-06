cff09323a98cc0e026a1f15a3fae9a7b8e50fa53
danil/// <d@d.com>
2022-09-06 16:18:16 +0300

    little refactoring

--- src/main/java/ru/yandex/practicum/filmorate/validators/FilmValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/validators/FilmValidator.java
@@ -6,7 +6,7 @@ import ru.yandex.practicum.filmorate.model.Film;
 import java.time.LocalDate;
 
 public class FilmValidator {
-    static private boolean checkName(Film film) {
+    private static boolean checkName(Film film) {
         if (film.getName() != null
                 && !film.getName().isEmpty()
                 && !film.getName().isBlank())
@@ -14,26 +14,26 @@ public class FilmValidator {
         throw new ValidationException("Invalid film's name");
     }
 
-    static private boolean checkYear(Film film) {
+    private static boolean checkYear(Film film) {
         LocalDate checkDate = LocalDate.of(1895, 12, 28);
         if (film.getReleaseDate().isAfter(checkDate))
             return true;
         throw new ValidationException("Invalid film's release date");
     }
 
-    static private boolean checkDescriptionLength(Film film) {
+    private static boolean checkDescriptionLength(Film film) {
         if (film.getDescription().length() <= 200)
             return true;
         throw new ValidationException("Invalid description length");
     }
 
-    static private boolean checkDuration(Film film) {
+    private static boolean checkDuration(Film film) {
         if (film.getDuration().isPositive())
             return true;
         throw new ValidationException("Invalid film's duration");
     }
 
-    static public boolean valid(Film film) {
+    public static boolean valid(Film film) {
         return (checkName(film)
                 && checkDescriptionLength(film)
                 && checkYear(film)
