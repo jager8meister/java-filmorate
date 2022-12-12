40e3ed9d60c388da22451e2efa73ac18d3fd56f3
danil/// <d@d.com>
2022-12-12 21:30:23 +0400

    finished UserDbStorage

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.filmorate.dao;
 
+import org.h2.jdbc.JdbcArray;
+import org.h2.value.ValueArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,6 +12,9 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storages.util.UserStorage;
 import ru.yandex.practicum.filmorate.validators.UserValidator;
 
+import java.sql.Array;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -77,6 +82,7 @@ public class UserDbStorage implements UserStorage {
             checkUserDuplicates(user);
             String res = insertData.executeAndReturnKey(user.toMap()).toString();
             user.setId(getId(user));
+
             return user;
         }
         throw new StorageException("Invalid user.");
@@ -91,7 +97,6 @@ public class UserDbStorage implements UserStorage {
             params.put("login", user.getLogin());
             params.put("name", user.getName());
             params.put("birthday", user.getBirthday());
-            params.put("friendsIds", user.getFriendsIds());
 
             String sqlQuery = "update usersBase set " +
                     "email = ?, login = ?, name = ?, birthday = ? " +
@@ -111,7 +116,17 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public void deleteUser(User user) {
-
+        User check = getUserById(user.getId());
+        if (check.getName().equals(user.getName()) &&
+            check.getLogin().equals(user.getLogin()) &&
+            check.getEmail().equals(user.getEmail()) &&
+            check.getBirthday().equals(user.getBirthday())) {
+            jdbcTemplate.queryForList("DELETE FROM usersBase WHERE id = " + user.getId());
+            jdbcTemplate.queryForList("DELETE FROM friends WHERE user_id = " + user.getId());
+            jdbcTemplate.queryForList("DELETE FROM friends WHERE friend_id = " + user.getId());
+        } else {
+            throw new StorageException("Invalid user");
+        }
     }
 
     @Override
@@ -132,16 +147,8 @@ public class UserDbStorage implements UserStorage {
         user.setLogin(elem.get("LOGIN").toString());
         user.setEmail(elem.get("EMAIL").toString());
         user.setId(Long.parseLong(elem.get("ID").toString()));
-        user.setFriendsIds(getFriendsIds(elem.get("FRIENDSIDS")));
+        user.setFriendsIds(getFriendsIds(user.getId()));
         return user;
-    }
-
-    private Set<Long> getFriendsIds(Object friends) {
-        Set<Long> res = new HashSet<>();
-        if (friends != null) {
-            String str = friends.getClass().toString();
-        }
-        return res;
     }
 
     @Override
@@ -159,21 +166,69 @@ public class UserDbStorage implements UserStorage {
 
     @Override
     public void addFriend(long id, long friendId) {
+        checkId(id);
+        checkId(friendId);
 
+        SimpleJdbcInsert insertData = new
+                SimpleJdbcInsert(jdbcTemplate).
+                withTableName("friends").
+                usingColumns("user_id", "friend_id")
+                .usingGeneratedKeyColumns("id");
+
+        Map<String, Long> params = new HashMap<>();
+        params.put("user_id", id);
+        params.put("friend_id", friendId);
+        insertData.execute(params);
+//        params = new HashMap<>();
+//        params.put("user_id", friendId);
+//        params.put("friend_id", id);
+//        insertData.execute(params);
+    }
+
+    private Set<Long> getFriendsIds(long id) {
+        Set<Long> ids = new HashSet<>();
+        List<Map<String, Object>> res = this.jdbcTemplate.queryForList("SELECT friend_id FROM friends WHERE USER_ID = " + id);
+        for (Map<String, Object> elem : res) {
+            Long friend = Long.parseLong(elem.get("friend_id").toString());
+            ids.add(friend);
+        }
+        return ids;
     }
 
     @Override
     public Collection<User> getAllFriends(long id) {
-        return null;
+        checkId(id);
+        List<Map<String, Object>> res = this.jdbcTemplate.queryForList("SELECT friend_id FROM friends WHERE USER_ID = " + id);
+        Set<User> ids = new HashSet<>();
+        for (Map<String, Object> elem : res) {
+            Long friend = Long.parseLong(elem.get("friend_id").toString());
+            ids.add(getUserById(friend));
+        }
+        return ids;
     }
 
     @Override
     public Collection<User> getCommonFriends(long id, long otherId) {
-        return null;
+        Collection<Long> friendIds = getUserById(id).getFriendsIds();
+        Collection<Long> otherFriendIds = getUserById(otherId).getFriendsIds();
+        Set<Long> resIds = new HashSet<>();
+        for (Long elemId: friendIds) {
+            if (otherFriendIds.contains(elemId))
+                resIds.add(elemId);
+        }
+        Collection<User> res = new HashSet<>();
+        for (Long elemId : resIds) {
+            res.add(getUserById(elemId));
+        }
+        return res;
     }
 
     @Override
     public User removeFriend(long id, long friendId) {
-        return null;
+        checkId(id);
+        checkId(friendId);
+        jdbcTemplate.execute("DELETE FROM friends WHERE user_id = " + id + " AND friend_id = " + friendId);
+        jdbcTemplate.execute("DELETE FROM friends WHERE user_id = " + friendId + " AND friend_id = " + id);
+        return getUserById(id);
     }
 }
